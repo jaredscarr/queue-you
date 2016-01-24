17702c1faffbe826edf7d19a11a418abbc005a06
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-23 16:37:08 -0800

    testing json added. Testing html form inputs added. testing js added.

--- scripts/testFirebase.js
+++ scripts/testFirebase.js
@@ -1,8 +1,30 @@
 var newPerson = new Firebase('https://queue-you.firebaseio.com/');
 
+//new profile constructor
 function NewProfile(first, last, email, course) {
   this.first = first;
   this.last = last;
   this.email = email;
   this.course = course;
+}
+
+//add user new user profile to the DOM as JSON data
+
+
+
+//populate current array of users with a callback function
+
+function populateUsers (NewProfile) {
+  $.getJSON('data/test.json', function(data) {
+
+  });
+}
+//separate teachers and students via the filter and map methods
+function separateUsers (totalUsers) {
+
+}
+
+function loadUsers (teachers, students) {
+  //add teachers to the DOM
+  //add students to the DOM
 }
