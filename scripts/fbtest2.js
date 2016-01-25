6a066bd96bed9b244893683452c23da95cc214f5
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-25 13:46:17 -0800

    Brandon Nav Jared Drove for all recent commits. Testing removed. code for Firebase in progress

--- scripts/fbtest2.js
+++ scripts/fbtest2.js
@@ -1,7 +1,6 @@
 var ref = new Firebase('https://queue-you.firebaseio.com/');
 
-
-var profiles = [{name : 'jared'}, {name : 'brandon'}, {name : 'Carson'}, {name : 'Michael'}];
+// var profiles = [{name : 'jared'}, {name : 'brandon'}, {name : 'Carson'}, {name : 'Michael'}];
 
 //this pushes the array of objects into the firebase database!!!!!
 // var pushProfiles = function() {
@@ -12,14 +11,22 @@ var profiles = [{name : 'jared'}, {name : 'brandon'}, {name : 'Carson'}, {name :
 //   );
 // }
 
-var newProfile = function (name) {
+function Profile (name, issue, desc) {
   this.name = name;
-}
+  this.issue = issue;
+  this.desc = desc;
+};
 
-var pushProfiles = function() {
-  profiles.forEach(function (user) {
-    console.log(user);
-    ref.push(user);
-    }
-  );
-}
+var getFormData = function() {
+  $('#').on('click', function(e) {
+    $name
+    $issue
+    $desc
+    var newUser = new Profile($name, $issue, $desc);
+    ref.push(newUser);
+  })
+};
+
+// var pushProfile = function(profile) {
+//   ref.push(profile);
+// };
