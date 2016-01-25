ad28b45653ce2ce9d658be90bd579255d9e21057
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-24 16:35:19 -0800

    playing around with the firebase methods. No success

--- scripts/testFirebase.js
+++ scripts/testFirebase.js
@@ -1,36 +1,65 @@
-(function(module) {
-  var theDatabase= {};
-  var newPerson = new Firebase('https://queue-you.firebaseio.com/database');
+// (function(module, firebase) {
+  var db= {};
+  var ref = new Firebase('https://queue-you.firebaseio.com/users');
 
-  //new profile constructor
-  theDatabase.NewProfile = function(first, last, email, course) {
-    this.first = first;
-    this.last = last;
+  //new profile contructor for basic level user
+  db.NewProfile = function(name, email, course) {
+    this.name = name;
     this.email = email;
     this.course = course;
-    // this.admin = admin;
-  }
+  };
 
-  //add user new user profile to the DOM as JSON data
+  db.createNewUser = function() {
+    var $name = $('#name').val();
+    var $email = $('#email').val();
+    var $course = $('select option:selected').val();
+    console.log($name, $email, $course);
+    var user = db.NewProfile($name, $email, $course);
+    return user;
+  }
+  //sign up event listener
+  db.signUp = function (user) {
+    $('#signup').on('click', function (e) {
+      ref.createUser(
+        user
+      , function(error, userData) {
+        if (error) {
+          console.log('Error creating user: ' + error);
+        }  else {
+            console.log('successfully created account with uid: ', userData.uid);
+        }
+      });
+    }
+  )};
+db.signUp();
+  //sign in event lister
+
+  //admin info?
+
+  //create user
+
+
+  //add user new user profile to the DOM as JSON db
 
 
 
   //populate current array of users with a callback function
 
-  theDatabase.populateUsers = function(NewProfile) {
-    $.getJSON('data/test.json', function(data) {
+  db.populateUsers = function(NewProfile) {
+    $.getJSON('db/test.json', function(db) {
 
     });
-  }
+  };
   //separate teachers and students via the filter and map methods
-  theDatabase.separateUsers = function(totalUsers) {
+  db.separateUsers = function(totalUsers) {
 
-  }
+  };
 
-  theDatabase.loadUsers = function(teachers, students) {
+  db.loadUsers = function(teachers, students) {
     //add teachers to the DOM
     //add students to the DOM
-  }
-  
-  module.theDatabase = theDatabase;
-})(window);
+  };
+
+  // module.theDatabase = theDatabase;
+  // firebase.ref = ref;
+// })(window);
