11472ee573c8e0a17f6a0f62771fa7d970e825f8
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-26 15:32:18 -0800

    fixed firebase reference so that we can retrieve data

--- scripts/teacherView.js
+++ scripts/teacherView.js
@@ -1,32 +1,33 @@
 // (function(module) {
 
   var teacherView = {};
-  var ref = new Firebase('https://queue-you.firebaseio.com/');
+  var ref = new Firebase('https://queue-you.firebaseio.com/users');
   var profileList = [];
 
   teacherView.showList = function() {
     $('#teacher-button').on('click', function() {
       $('#teacher').show().siblings().hide();
-      teacherView.post();
+      // teacherView.post();
     });
   };
 
   teacherView.storagePull = function(){
     ref.on('child_added', function(snapshot, prevChildKey){
-      var newPost = snapshot.val();
-      console.log(newPost);
-      profileList.push(snapshot.val());
+      var newStudent = snapshot.val();
+      console.log(newStudent);
+      // profileList.push(snapshot.val());
+      console.log('Name: ' + newStudent.name);
     });
   };
 
 
-  teacherView.post = function(){
-    for (var i = 0; i < profileList.length; i++) {
-      $('ol').append('<li>Name: ' + profileList[i].name + '</br>' +
-      profileList[i].issue + '</br>' +
-      profileList[i].desc +'</br>' + '</br>' + '</li');
-    }
-  };
+  // teacherView.post = function(){
+  //   for (var i = 0; i < profileList.length; i++) {
+  //     $('ol').append('<li>Name: ' + profileList[i].name + '</br>' +
+  //     profileList[i].issue + '</br>' +
+  //     profileList[i].desc +'</br>' + '</br>' + '</li');
+  //   }
+  // };
   //   profileList.forEach(function(name, issue, desc) {
   //     $('ol').append('<li>' + this.name + this.issue + this.desc + '</li>').toHtml();
   //   });
