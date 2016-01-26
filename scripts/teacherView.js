9c8c7137faf176267383612ce97a9122076a793d
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-26 09:37:44 -0800

    displaying database objects to console

--- scripts/teacherView.js
+++ scripts/teacherView.js
@@ -1,6 +1,13 @@
 (function(module) {
 
   var teacherView = {};
+  var ref = new Firebase('https://queue-you.firebaseio.com/');
+
+  ref.on('value', function(snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.lgot('The read failed: ' + errorObject.code);
+  });
 
   teacherView.showList = function() {
     $('#teacher-button').on('click', function() {
