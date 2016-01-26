37893b650fe36c6cf5bb194fc7ac2fd2ea16cf93
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-26 11:11:59 -0800

    modified teacherView.js, grabbing data from firebase and pushing into array of objects

--- scripts/teacherView.js
+++ scripts/teacherView.js
@@ -1,13 +1,8 @@
-(function(module) {
+// (function(module) {
 
   var teacherView = {};
-  var ref = new Firebase('https://queue-you.firebaseio.com/');
-
-  ref.on('value', function(snapshot) {
-    console.log(snapshot.val());
-  }, function (errorObject) {
-    console.lgot('The read failed: ' + errorObject.code);
-  });
+  var ref = new Firebase('https://queue-you.firebaseio.com/users');
+  var profileList = [];
 
   teacherView.showList = function() {
     $('#teacher-button').on('click', function() {
@@ -15,7 +10,18 @@
     });
   };
 
-  teacherView.showList();
 
-  module.teacherView = teacherView;
-})(window);
+  teacherView.storagePull = function(){
+    ref.on('child_added', function(snapshot, prevChildKey){
+      var newPost = snapshot.val();
+      console.log(newPost);
+      profileList.push(snapshot.val());
+    });
+  };
+
+
+  teacherView.showList();
+  teacherView.storagePull();
+
+//   module.teacherView = teacherView;
+// })(window);
