5d508f9f6d913ec7fe0caa6d9aec0ed34a0afa20
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-28 08:44:56 -0800

    deleted testing script file. Added functionality to list switching. Not working yet waiting to add html ids.

--- scripts/teacherView.js
+++ scripts/teacherView.js
@@ -1,70 +1,65 @@
-// (function(module) {
+var teacherView = {};
+var ref = new Firebase('https://queue-you.firebaseio.com/');
+var ref2 = new Firebase('https://queue-you.firebaseio.com/users/');
+var profileList = [];
+var keys = [];
 
-  var teacherView = {};
-  var ref = new Firebase('https://queue-you.firebaseio.com/');
-  var ref2 = new Firebase('https://queue-you.firebaseio.com/users/');
-  // var ref3 = new Firebase('https://queue-you.firebaseio.com/users/-K93F-mA76dCgyazMeEG');
-  var profileList = [];
-  var keys = [];
+// need to add to html:
+// 1. a div inside teacher view below the
+// ol tag that says current and has a ul tag with the id of chosen-one
+// p tag that says click to remove.
 
-  teacherView.showList = function() {
-    $('#teacher-button').on('click', function() {
-      $('#teacher').show().siblings().hide();
-      teacherView.post();
-    });
-  };
+teacherView.showList = function() {
+  $('#teacher-button').on('click', function(e) {
+    e.preventDefault();
+    $('#teacher').show().siblings().hide();
+    $('#chosen-one').hide();
+    $('#current').hide();
+    teacherView.post();
+  });
+};
 
+teacherView.storagePull = function(){
+  ref2.on('child_added', function(snapshot, prevChildKey){
+    var newPost = snapshot.val();
+    profileList.push(snapshot.val());
+    keys.push(snapshot.key());
+    console.log(snapshot.key());
+    // storeLocal(userid);
+  });
+};
+teacherView.post = function(){
+  for (var i = 0; i < profileList.length; i++) {
+    $('ol').append('<li>Name: ' + profileList[i].name + '</br>' +
+    profileList[i].issue + '</br>' +
+    profileList[i].desc +'</br>' + '</br>' + '</li>');
+    teacherView.selected();
+  }
+};
 
-  // /////////////////////////////////////
-  //
-  //
-  // teacherView.test = function() {
-  //   $('#teacher-test').on('click', function() {
-  //     // e.preventDefault();
-  //     $('#form').show().siblings().hide();
-  //   });
-  // };
-  //
-  //
-  // /////////////////////////////////////
+teacherView.removeFromdb = function() {
+  $('#chosen').on('click', function(e) { //on button click remove from db
+    e.preventDefault();
+    var $key = $(this).index();
+    console.log($key);
+    var deleteRef = new Firebase('https://queue-you.firebaseio.com/users/' + keys[$key]);
+    deleteRef.remove();
+    $(this).remove();
+  });
+};
 
+teacherView.selected = function() {
+  $('li').on('click', function(e) {
+    e.preventDefault();
+    var $chosen = $(this).clone();
+    $(this).hide();
+    $('#current').show();
+    $('#chosen-one').append($chosen).show();
+    $('#chosen-one li').attr('id', 'chosen');
+    teacherView.removeFromdb();
+  });
+};
 
-  teacherView.storagePull = function(){
-    ref2.on('child_added', function(snapshot, prevChildKey){
-      var newPost = snapshot.val();
-      // console.log(newPost);
-      // console.log(snapshot.key());
-      profileList.push(snapshot.val());
-      keys.push(snapshot.key());
-    });
-  };
-  teacherView.post = function(){
-    for (var i = 0; i < profileList.length; i++) {
-      $('ol').append('<li>Name: ' + profileList[i].name + '</br>' +
-      profileList[i].issue + '</br>' +
-      profileList[i].desc +'</br>' + '</br>' + '</li>');
-      teacherView.remove();
-    }
-  };
-
-  teacherView.remove = function() {
-    $('li').click(function() {
-      var $key = $(this).index();
-      console.log($key);
-      console.log(keys[$key]);
-      var deleteRef = new Firebase('https://queue-you.firebaseio.com/users/' + keys[$key]);
-      console.log(deleteRef);
-      console.log($(this).index());
-      deleteRef.remove();
-      $(this).remove();
-
-
-      // ref2.child.key.remove();
-    });
-  };
-
-  teacherView.showList();
-  teacherView.storagePull();
-  // teacherView.test();
-//   module.teacherView = teacherView;
-// })(window);
+teacherView.showList();
+teacherView.selected();
+teacherView.storagePull();
